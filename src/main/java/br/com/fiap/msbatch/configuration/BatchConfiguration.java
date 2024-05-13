5c05ce476ac441948381014886e58bbf5c93cd68
edujguerra <edujguerra@gmail.com>
2024-05-13 14:51:44 -0300

    01-Commit inicial

---
+++ src/main/java/br/com/fiap/msbatch/configuration/BatchConfiguration.java
@@ -0,0 +1,85 @@
+package br.com.fiap.msbatch.configuration;
+
+import br.com.fiap.msbatch.model.Produto;
+import br.com.fiap.msbatch.processor.ProdutoProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfiguration {
+
+    @Bean
+    public Job processarProduto(JobRepository jobRepository, Step step){
+
+        return new JobBuilder("importProduto", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository,
+                     PlatformTransactionManager platformTransactionManager,
+                     ItemReader<Produto> itemReader,
+                     ItemWriter<Produto> itemWriter,
+                     ItemProcessor<Produto,Produto> itemProcessor){
+
+        return new StepBuilder("step", jobRepository)
+                .<Produto,Produto>chunk(20, platformTransactionManager)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Produto> itemReader(){
+        BeanWrapperFieldSetMapper<Produto> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Produto.class);
+
+        return new FlatFileItemReaderBuilder<Produto>()
+                .name("presonItemReader")
+                .resource(new ClassPathResource("produto.csv"))
+                .delimited()
+                .names("id", "nome", "descricao", "quantidade", "preco")
+                .fieldSetMapper(fieldSetMapper)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Produto> itemWriter(DataSource dataSource){
+        return new JdbcBatchItemWriterBuilder<Produto>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .dataSource(dataSource)
+                .sql("INSERT INTO tb_produtos "
+                    + "(id_produto, nm_produto, ds_descricao, qt_estoque, pr_produto)"
+                    + "values(:id, :nome, :descricao, :quantidade, :preco)"
+                    + "ON DUPLICATE KEY UPDATE nm_produto=:nome,"
+                    + " ds_descricao=:descricao, qt_estoque=:quantidade, pr_produto=:preco"
+                )
+                .build();
+
+    }
+
+    @Bean
+    public ItemProcessor<Produto, Produto> itemProcessor(){
+        return new ProdutoProcessor();
+    }
+}
---
+++ src/main/java/br/com/fiap/msbatch/processor/ProdutoProcessor.java
@@ -0,0 +1,18 @@
+package br.com.fiap.msbatch.processor;
+
+import br.com.fiap.msbatch.model.Produto;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDateTime;
+
+public class ProdutoProcessor implements ItemProcessor<Produto,Produto> {
+
+    @Override
+    public Produto process(Produto item) throws Exception {
+
+        item.setDataUpdate(LocalDateTime.now());
+        return item;
+    }
+
+
+}
